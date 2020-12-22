69a9773e99823019052751c5e5c8ebccdc43bc5b
Finn Klapper <finnklapper@Finns-MacBook-Pro.local>
2020-12-23 00:08:27 +0100

    Transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,14 +20,47 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Shirt', amount: 29.99, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'New Pants', amount: 89.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
       ),
-      body: Center(
-        child: Text('Widget Playground'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Center(child: Text('CHART!')),
+            elevation: 5,
+          ),
+          Column(
+            children: transactions.map((e) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(e.amount.toString()),
+                    ),
+                    Column(
+                      children: [Text(e.title), Text(e.date.toString())],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
