b814ba9446b1437dab88fe7bda5a3f1e71cf879d
PyFinn <realstraved@gmail.com>
2020-12-24 22:15:22 +0100

    Modal Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'widgets/user_transaction.dart';
-
+import 'package:personal_expenses/transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,13 +21,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Shirt', amount: 29.99, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'New Pants', amount: 89.99, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String title, String amount) {
+    setState(() {
+      _transactions.add(Transaction(
+          id: 't4',
+          title: title,
+          amount: double.parse(amount),
+          date: DateTime.now()));
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return GestureDetector(
+            child: NewTransaction(_addNewTransaction),
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Personal Expenses'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -37,9 +78,16 @@ class MyHomePage extends StatelessWidget {
               child: Center(child: Text('CHART!')),
               elevation: 5,
             ),
-            UserTransactions()
+            TransactionList(_transactions)
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          _startAddNewTransaction(context);
+        },
       ),
     );
   }
