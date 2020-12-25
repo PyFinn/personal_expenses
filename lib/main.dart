4d97b60cca598438b5deb10c5ab362bdfe9a621b
PyFinn <realstraved@gmail.com>
2020-12-25 19:01:17 +0100

    Chart Backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:personal_expenses/transaction.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,13 +37,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    // Transaction(
-    //     id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    // Transaction(
-    //     id: 't2', title: 'New Shirt', amount: 29.99, date: DateTime.now()),
-    // Transaction(
-    //     id: 't3', title: 'New Pants', amount: 89.99, date: DateTime.now())
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Shirt', amount: 29.99, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'New Pants', amount: 89.99, date: DateTime.now())
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((element) => element.date.isAfter(DateTime.now().subtract(Duration(days: 7)))).toList();
+  }
 
   void _addNewTransaction(String title, String amount) {
     setState(() {
@@ -81,11 +86,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Card(
-              color: Colors.blue,
-              child: Center(child: Text('CHART!')),
-              elevation: 5,
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions)
           ],
         ),
