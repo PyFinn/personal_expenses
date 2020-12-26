55e020d4aebaf5644ae5fd23c75fdc624dc8d61d
PyFinn <realstraved@gmail.com>
2020-12-26 16:35:45 +0100

    List Tile Widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -15,7 +15,13 @@ class TransactionList extends StatelessWidget {
                 'No transactions added yet!',
                 style: Theme.of(context).textTheme.headline6,
               ),
-              Container(child: Image.asset('assets/image/waiting.png', fit: BoxFit.cover,), height: 200,)
+              Container(
+                child: Image.asset(
+                  'assets/image/waiting.png',
+                  fit: BoxFit.cover,
+                ),
+                height: 200,
+              )
             ],
           )
         : Container(
@@ -23,38 +29,21 @@ class TransactionList extends StatelessWidget {
             child: ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Theme.of(context).primaryColor),
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                                width: 2)),
-                        padding: EdgeInsets.all(10),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(transactions[index].date),
-                            style: Theme.of(context).textTheme.headline1,
-                          ),
-                        ],
-                      )
-                    ],
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                          padding: EdgeInsets.all(6),
+                          child: FittedBox(
+                              child: Text('\$${transactions[index].amount}'))),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(DateFormat.yMMMd().format(transactions[index].date)),
                   ),
                 );
               },
